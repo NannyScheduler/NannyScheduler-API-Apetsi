4ed2b2a5bf17d84cc386b011d0e8e2d24b0199d7
aapetsi <apetsiampiah@gmail.com>
2019-11-22 05:18:25 +0000

    Modify db table

--- data/migrations/20191118140741_create-nannies-table.js
+++ data/migrations/20191118140741_create-nannies-table.js
@@ -1,13 +1,23 @@
 
 exports.up = function (knex) {
-  return knex.schema.createTable('nannies', nannies => {
-    nannies.increments('nanny_id')
-    nannies.string('username').unique().notNullable()
-    nannies.string('email').unique().notNullable()
-    nannies.string('password').notNullable()
-  })
+  return knex.schema
+    .createTable('nannies', nannies => {
+      nannies.increments('nanny_id')
+      nannies.string('username').unique().notNullable()
+      nannies.string('email').unique().notNullable()
+      nannies.string('password').notNullable()
+    })
+    .createTable('profile', profile => {
+      profile.increments('profile_id').primary()
+      profile.integer('nanny_id').references('nanny_id').inTable('nannies').notNull().onDelete('cascade')
+      profile.string('address').notNullable()
+      profile.integer('phone_number').notNullable()
+      profile.boolean('drive_duties').defaultTo(false)
+    })
 }
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists('nannies')
+  return knex.schema
+    .dropTableIfExists('profile')
+    .dropTableIfExists('nannies')
 }
